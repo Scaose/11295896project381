df119cc42084c53cb14600ff35f9e3df231662bd
Scaose <scaose0326@gmail.com>
2015-11-29 13:56:27 +0800

    First azure test

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var app = express();
-var MONGODBURL = 'mongodb://localhost:27017/test';
+//var MONGODBURL = 'mongodb://localhost:27017/test';
+var MONGODBURL = 'mongodb://admin:password@ds054298.mongolab.com:54298/restaurants';
 
 var restSchema = require('./models/restaurant.js');
 app.use(express.static('public'));
@@ -11,6 +12,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   res.sendfile('html/main.html');	
+});
+
+
+//search.html
+app.get('/search', function(req,res) {
+	res.sendFile(__dirname + '/html/search.html'); 
 });
 
 //create.ejs
@@ -25,7 +32,7 @@ app.get('/update/:reqName', function(req, res){
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function (callback){
-	  var restaurant = mongoose.model('MiniRestaurant', restSchema);
+	  var restaurant = mongoose.model('Restaurant', restSchema);
 		restaurant.find({name:req.params.reqName}, function(err, results){
 		  if(err)
 		    res.write("<p>Error: " + err.message + "</p>");
@@ -37,7 +44,173 @@ app.get('/update/:reqName', function(req, res){
 	});
 });
 
-//Create action
+function CreateRestaurantObj(body){
+	var obj = {};
+	obj.address = {};
+	obj.address.coord = [];
+	if(body.building != null)
+		obj.address.building = body.building;
+	if(body.lon != null)
+		obj.address.coord.push(parseInt(body.lon));
+	else
+		obj.address.coord.push(null);
+	if(body.lan != null)
+		obj.address.coord.push(parseInt(body.lan));
+	else
+		obj.address.coord.push(null);
+	if(body.street != null)
+		obj.address.street = body.street;
+	if(body.zipcode != null)
+		obj.address.zipcode = body.zipcode;
+	if(body.borough != null)
+		obj.borough = body.borough;
+	if(body.cuisine != null)
+		obj.cuisine = body.cuisine;
+	if(body.name != null)
+		obj.name = body.name;
+	if(body.restaurant_id != null)
+		obj.restaurant_id = body.restaurant_id;
+	return obj;
+}
+
+function MergeRestaurantObj(oldObj, newObj){
+	if(newObj.address.building != null)
+		oldObj.address.building = newObj.building;
+	if(newObj.address.coord[0] != null){
+		oldObj.address.coord[0] = parseInt(newObj.address.coord[0]);
+	}
+	if(newObj.address.coord[1] != null){
+		oldObj.address.coord[1] = parseInt(newObj.address.coord[1]);
+	}
+	if(newObj.address.street != null)
+		oldObj.address.street = newObj.street;
+	if(newObj.address.zipcode != null)
+		oldObj.address.zipcode = newObj.zipcode;
+	if(newObj.borough != null)
+		oldObj.borough = newObj.borough;
+	if(newObj.cuisine != null)
+		oldObj.cuisine = newObj.cuisine;
+	if(newObj.name != null)
+		oldObj.name = newObj.name;
+	if(newObj.restaurant_id != null)
+		oldObj.restaurant_id = newObj.restaurant_id;
+	return oldObj;
+}
+
+//curl create
+app.post('/', function(req,res) {
+	console.log("Create from curl");
+	mongoose.connect(MONGODBURL);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		console.log("Connect to DB");
+		var restaurant = mongoose.model('Restaurant', restSchema);
+		var datajson = CreateRestaurantObj(req.body);
+		var r = new restaurant(datajson);
+		r.save(function(err) {
+			if (err) {
+				res.status(500).json(err);
+				throw err;
+			}
+			res.status(200).json({message: "insert done", restaurant_id : req.body.restaurant_id});
+			console.log('A model is Created');
+
+			res.end();
+			db.close();
+		});
+	});
+});
+
+//curl delete
+app.delete('/:field/:value', function(req, res){
+	console.log("Delete From curl");
+	mongoose.connect(MONGODBURL);
+        var db = mongoose.connection;
+	var datajson = {};
+	datajson[req.params.field] = req.params.value;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function (callback){
+	  var restaurant = mongoose.model('Restaurant', restSchema);
+		restaurant.findOneAndRemove(datajson, function(err){
+			  if(err){
+			    res.status(500).json(err);
+			    throw err;
+			  }
+			  db.close();
+			  res.status(200).json({message: "delete done", restaurant_id : req.params.id});
+			  res.end(); 
+			});
+
+		
+	});
+});
+
+//curl Update
+app.put('/:field/:value', function(req, res){
+	console.log("Update From curl");
+	var jsoncondition={};
+	jsoncondition[req.params.field] = req.params.value;
+	mongoose.connect(MONGODBURL);
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function (callback){
+	  var restaurant = mongoose.model('Restaurant', restSchema);
+		restaurant.findOne(jsoncondition, function(err, result){
+			  if(err){
+			    res.status(500).json(err);
+			    throw err;
+			  }
+			  var jsonupdatedata = CreateRestaurantObj(req.body);
+			  jsonupdatedata = MergeRestaurantObj(result, jsonupdatedata);
+			  //result = jsonupdatedata;
+			  //result.save(); <-- I don't know why this function cannot save coord
+			  //a lazy function to solve the above problem...
+			  restaurant.update(jsoncondition, jsonupdatedata, function(err){
+				    if(err){
+				      res.status(500).json(err);
+				      throw err;
+				    }
+				  db.close();
+				  var msg = {message: "Update done"};
+				  msg[req.params.field] = req.params.value;
+				  res.status(200).json(msg);
+				  res.end(); 
+			  });
+		});
+				
+	});
+});
+
+//curl Read
+app.get('/:field/:value', function(req,res) {
+	console.log("Search get it");
+	var datajson = {};
+	datajson[req.params.field] = req.params.value;
+        mongoose.connect(MONGODBURL);
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function (callback){
+	  var restaurant = mongoose.model('Restaurant', restSchema);
+		restaurant.find(datajson, function(err, results){
+		  if(err){
+		    res.status(500).json(err);
+		    throw err;
+		  }
+		  db.close();
+		  if(results.length>0){	//Has result	   
+		      res.status(200).json(results);
+		  }
+		  else{			//No result
+		      res.status(200).json({message:"No matching document"});
+		  }
+		  res.end(); 
+		});
+		    
+	});
+});
+
+//Create action for web
 app.post('/newRest', function(req,res) {
 	console.log("Create get it");
 	//var restData = {name: ""};
@@ -48,7 +221,7 @@ app.post('/newRest', function(req,res) {
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
 		console.log("Connect to DB");
-		var restaurant = mongoose.model('MiniRestaurant', restSchema);
+		var restaurant = mongoose.model('Restaurant', restSchema);
 		var r = new restaurant({address:{building: req.body.building,
 				         coord: [parseInt(req.body.lon),parseInt(req.body.lat)],
 				         street: req.body.street,
@@ -77,19 +250,14 @@ app.post('/newRest', function(req,res) {
 	});
 });
 
-//search
-app.get('/search', function(req,res) {
-	res.sendFile(__dirname + '/html/search.html'); 
-});
-
-//Read action
+//Read action for web
 app.get('/display', function(req,res) {
 	console.log("Read get it");
         mongoose.connect(MONGODBURL);
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function (callback){
-	  var restaurant = mongoose.model('MiniRestaurant', restSchema);
+	  var restaurant = mongoose.model('Restaurant', restSchema);
 		restaurant.find({}, function(err, results){
 		  if(err)
 		    res.write("<p>Error: " + err.message + "</p>");
@@ -101,7 +269,36 @@ app.get('/display', function(req,res) {
 	});
 });
 
-//Update action
+//Search action for web
+app.get('/searchRest', function(req,res) {
+	console.log("Search from web " + req.body.field + " " + req.body.value);
+	var datajson = {};
+	datajson[req.query.field] = req.query.value;
+        mongoose.connect(MONGODBURL);
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function (callback){
+	  var restaurant = mongoose.model('Restaurant', restSchema);
+		restaurant.find(datajson, function(err, results){
+		  if(err){
+		    res.status(500).json(err);
+		    throw err;
+		  }
+		  db.close();
+		  if(results.length>0){	//Has result	   
+		     	res.render('display',  {restResult : results}); 
+		  }
+		  else{			//No result
+		      
+			res.write("<p>There is no any matching result.</p>");
+		  }
+		  res.end(); 
+		});
+		    
+	});
+});
+
+//Update action for web
 app.put('/updateRest/:id/:value', function(req, res){
 	console.log("Put get it");
 	var data = req.params.value.split("!@&");
@@ -118,7 +315,7 @@ app.put('/updateRest/:id/:value', function(req, res){
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function (callback){
-	  var restaurant = mongoose.model('MiniRestaurant', restSchema);
+	  var restaurant = mongoose.model('Restaurant', restSchema);
 		restaurant.findOneAndUpdate({restaurant_id
 :req.params.id},jsondata, {upsert:false}, function(err){
 		  if(err)
@@ -131,14 +328,14 @@ app.put('/updateRest/:id/:value', function(req, res){
 	});
 });
 
-//Delete action 
+//Delete action for web
 app.delete('/deleteRest/name/:id', function(req, res){
 	console.log("Delete Get it");
 	mongoose.connect(MONGODBURL);
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function (callback){
-	  var restaurant = mongoose.model('MiniRestaurant', restSchema);
+	  var restaurant = mongoose.model('Restaurant', restSchema);
 		restaurant.findOneAndRemove({restaurant_id:req.params.id}, function(err){
 			  if(err)
 			    res.write("<p>Error: " + err.message + "</p>");
@@ -149,30 +346,6 @@ app.delete('/deleteRest/name/:id', function(req, res){
 		
 	});
 });
-
-/*app.get('/searchRest',function(req,res) {
-
-mongoose.connect(MONGODBURL);
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-		var restaurant = mongoose.model('Restaurant', restSchema);
-		var criteria = {};
-		if (req.query.name) {criteria.name = RegExp(req.query.name)}
-		restaurant.find(criteria, function(err,results) {
-			if (err) {
-				console.log("Error: " + err.message);
-				res.write(err.message);
-			}
-			else {
-				console.log('Found: ',results.length);
-				res.render('showEditRest',{restaurants: results});
-			}
-			res.end();
-			db.close();
-		});
-	});
-});*/
 
 
 
